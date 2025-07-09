9160532570f0b849ac68e7969dddba332b67364e
Matheus Mendes <104966152+MendesMat@users.noreply.github.com>
2025-07-09 20:21:30 -0300

    Inverter Behaviour

---
+++ Assets/Scripts/Core/BehaviourTree/Inverter.cs
@@ -0,0 +1,22 @@
+namespace Core.BehaviourTree
+{
+    public class Inverter : Node
+    {
+        public Inverter(string name) : base(name) { }
+
+        public override Status Process()
+        {
+            switch (ChildrenList[0].Process())
+            {
+                case Status.Running:
+                    return Status.Running;
+                
+                case Status.Failure:
+                    return Status.Success;
+                
+                default:
+                    return Status.Failure;
+            }
+        }
+    }
+}
